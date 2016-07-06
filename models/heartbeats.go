e626c5ea239f4e78e930e7f803b8a3392f4ab1d7
fudali <fudali.doob@gmail.com>
2016-07-07 02:31:21 +0800

    修复第一次注册pc时进度条不滑动

--- models/heartbeats.go
+++ models/heartbeats.go
@@ -1,28 +1,26 @@
 package models
 
-import(
-
+import (
+	"encoding/json"
 	"fmt"
 	"time"
-	"encoding/json"
-
 )
 
-type HeartBeats struct{
-	Cid string `json:"pc_id"`
-	HeartBeats int `json:"hb"`
+type HeartBeats struct {
+	Cid        string `json:"pc_id"`
+	HeartBeats int    `json:"hb"`
 }
 
 var History map[string]int64
 
-const(
+const (
 	checkTimer = time.Second * 5
 )
 
 func RecordPcLastTime(pcstatus []byte) {
-	s := &HeartBeats{HeartBeats:-1}
-	err := json.Unmarshal(pcstatus,s)
-	if err!=nil {
+	s := &HeartBeats{HeartBeats: -1}
+	err := json.Unmarshal(pcstatus, s)
+	if err != nil {
 		fmt.Println(err)
 	}
 	if s.HeartBeats == 0 {
@@ -30,11 +28,27 @@ func RecordPcLastTime(pcstatus []byte) {
 		sendPcDown(s)
 	}
 	pcid := s.Cid
+	if notIn(pcid) {
+		go func() {
+			time.Sleep(time.Millisecond * 500)
+			Messages <- string(pcstatus)
+		}()
+	}
 	if pcid != "" {
 		nowTime := time.Now().Unix()
 		History[pcid] = nowTime
-		sendPcDown(&HeartBeats{Cid:pcid,HeartBeats:1})	
+		sendPcDown(&HeartBeats{Cid: pcid, HeartBeats: 1})
 	}
+}
+
+func notIn(id string) bool {
+	var count = 0
+	for k, _ := range History {
+		if k == id {
+			count = count + 1
+		}
+	}
+	return count == 0
 }
 
 func checkHB() {
@@ -42,26 +56,26 @@ func checkHB() {
 	t2 := time.NewTimer(time.Second * 10)
 
 	for {
-        select {
+		select {
 
-        case <-t1.C:
-        	for k,v := range History {
+		case <-t1.C:
+			for k, v := range History {
 				nowTime := time.Now().Unix()
-				missTime := nowTime-v
+				missTime := nowTime - v
 				if missTime > 10 {
-					sendPcDown(&HeartBeats{Cid:k,HeartBeats:0})
+					sendPcDown(&HeartBeats{Cid: k, HeartBeats: 0})
 				}
 			}
-            t1.Reset(checkTimer)
+			t1.Reset(checkTimer)
 
-        case <-t2.C:
-            t2.Reset(time.Second * 10)
-        }
-    }
+		case <-t2.C:
+			t2.Reset(time.Second * 10)
+		}
+	}
 }
 
 func sendPcDown(hb *HeartBeats) {
-	hbjson,_ := json.Marshal(hb)
+	hbjson, _ := json.Marshal(hb)
 	Messages <- string(hbjson)
 }
 
